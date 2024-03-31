8e067e2225a37dfa35b60911a5c3778a604e4f99
Lynn Anastasia <43697008+AnnaL001@users.noreply.github.com>
2024-03-31 18:51:44 +0300

    :sparkles: feat: add app navigation logic

---
+++ core/navigation/src/main/java/com/anna/greeneats/navigation/main/AppRoutes.kt
@@ -0,0 +1,10 @@
+package com.anna.greeneats.navigation.main
+
+/**
+ * All feature flows within the app
+ */
+object AppRoutes {
+  const val AUTH_FLOW = "authFlow"
+  const val HOME_FLOW = "homeFlow"
+  const val COOKED_RECIPES_FLOW = "cookedRecipesFlow"
+}
