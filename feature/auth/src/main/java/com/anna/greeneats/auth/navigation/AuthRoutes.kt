7da1459c9bfb3073be271be10f1c7d8e0b5333ae
Lynn Anastasia <43697008+AnnaL001@users.noreply.github.com>
2024-03-31 18:52:27 +0300

    :sparkles: feat: add app auth module

---
+++ feature/auth/src/main/java/com/anna/greeneats/auth/navigation/AuthRoutes.kt
@@ -0,0 +1,12 @@
+package com.anna.greeneats.auth.navigation
+
+/**
+ * Feature flows and destinations navigated to within/from auth module
+ */
+object AuthRoutes{
+  const val ID = "authFlow"
+  const val LOGIN = "login"
+  const val SIGN_UP = "signup"
+  const val LANDING = "landing"
+  const val HOME = "homeFlow"
+}
