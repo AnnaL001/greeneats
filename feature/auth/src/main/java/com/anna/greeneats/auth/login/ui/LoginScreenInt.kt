1c115d2d5345169bd2a5f3df321a4620472c13b7
Lynn Anastasia <43697008+AnnaL001@users.noreply.github.com>
2024-07-18 01:25:09 +0300

    :recycle: refactor: update viewmodel access

--- feature/auth/src/main/java/com/anna/greeneats/auth/login/ui/LoginScreenInt.kt
+++ feature/auth/src/main/java/com/anna/greeneats/auth/login/ui/LoginScreenInt.kt
@@ -1,5 +1,6 @@
 package com.anna.greeneats.auth.login.ui
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,6 +8,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.anna.greeneats.auth.login.state.LoginScreenEvents
 import com.anna.greeneats.auth.login.state.LoginScreenViewModel
@@ -15,28 +17,24 @@ import com.anna.greeneats.auth.login.state.LoginScreenViewModel
 @Composable
 internal fun LoginScreenInt(
     onNavigateToSignup: () -> Unit = {},
-    onNavigateToHome: () -> Unit = {},
-    loginModel: LoginScreenViewModel = viewModel()
+    onNavigateToHome: () -> Unit = {}
 ) {
   Column(modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.surface)) {
+    val loginModel = hiltViewModel<LoginScreenViewModel>()
     val loginState = loginModel.loginState.value
 
     LoginScreenUI(
-      loginState = loginState,
-      onSignUpNavigation = onNavigateToSignup,
-      onPasswordVisibilityToggle = {
-         loginModel.onEvents(LoginScreenEvents.OnPasswordVisibilityToggle)
-      },
-      onLoginValidation = {
-        loginModel.onEvents(LoginScreenEvents.OnLoginValidation)
-      },
-      onEmailChange = { email ->
-        loginModel.onEvents(LoginScreenEvents.OnEmailChange(email))
-      },
-      onPasswordChange = { password ->
-        loginModel.onEvents(LoginScreenEvents.OnPasswordChange(password))
-      },
-      onNavigateToHome = onNavigateToHome,
-      onLogin = { loginModel.onEvents(LoginScreenEvents.OnLogin)})
+        loginState = loginState,
+        onSignUpNavigation = onNavigateToSignup,
+        onPasswordVisibilityToggle = {
+          loginModel.onEvents(LoginScreenEvents.OnPasswordVisibilityToggle)
+        },
+        onLoginValidation = { loginModel.onEvents(LoginScreenEvents.OnLoginValidation) },
+        onEmailChange = { email -> loginModel.onEvents(LoginScreenEvents.OnEmailChange(email)) },
+        onPasswordChange = { password ->
+          loginModel.onEvents(LoginScreenEvents.OnPasswordChange(password))
+        },
+        onNavigateToHome = onNavigateToHome,
+        onLogin = { loginModel.onEvents(LoginScreenEvents.OnLogin) })
   }
 }
