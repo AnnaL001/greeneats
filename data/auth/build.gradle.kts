cc23f6f42d7b10db50d07443b6d534ef8fb3e57e
Lynn Anastasia <43697008+AnnaL001@users.noreply.github.com>
2024-09-15 22:08:05 +0300

    :wrench: build: integrate google authentication dependencies

--- data/auth/build.gradle.kts
+++ data/auth/build.gradle.kts
@@ -1,15 +1,41 @@
 import com.anna.greeneats.constants.AndroidModules
+import java.util.Properties
 
 plugins {
   alias(libs.plugins.greeneats.convention.data)
 }
 
+// Load api.properties file
+val apiProperties = Properties()
+val apiPropertiesFile = rootProject.file("api.properties")
+
+if (apiPropertiesFile.exists()) {
+  apiPropertiesFile.inputStream().use { inputStream ->
+    apiProperties.load(inputStream)
+  }
+}
+
+val googleClientId: String? = apiProperties.getProperty("GOOGLE_WEB_CLIENT_ID")
+
 android {
   namespace = "com.anna.greeneats.data.auth"
+
+  defaultConfig {
+    // Ensure myApiKey is non-null and then use it
+    googleClientId?.let {
+      buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"$it\"")
+    }
+  }
 }
 
 dependencies {
   implementation(project(AndroidModules.Core.MODEL))
   implementation(platform(libs.firebase.bom))
   implementation(libs.firebase.auth)
+  implementation(libs.google.play)
+  implementation(libs.androidx.credentials)
+  implementation(libs.androidx.credentials.play)
+  implementation(libs.identity.googleid)
+  implementation(libs.androidx.junit.ktx)
+  androidTestImplementation(libs.junit.junit)
 }
